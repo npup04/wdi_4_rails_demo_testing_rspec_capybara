8cfeef3c22f198240eaad9b70a4b248b84e50eb2
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-02-07 15:09:51 -0500

    Complete spec/models/comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Comment do
+  let(:valid_article_attribs) {
+    { title: 'One Stupid Trick', body: "You won't believe what happens next..." }
+  }
+  let(:article) {
+     Article.create!(valid_article_attribs)
+  }
+
+  before(:all) do
+    Comment.destroy_all
+    Article.destroy_all
+  end
+
   describe '.create' do
     it 'creates a new comment' do
-      @article = Article.create()
-      expect(@article.comments.create()).to be_a Comment
+      expect(article.comments.create()).to be_a Comment
     end
 
-  end
+    it 'is valid with valid body content' do
+      expect(article.comments.create(body: 'Amaaaazing body content')).to be_valid
+    end
+
+    it 'is invalid without a body field' do
+      expect(article.comments.create()).not_to be_valid
+    end
+
+    it 'is invalid with a nil body' do
+      expect(article.comments.create(body: nil)).not_to be_valid
+    end
+
+  end # describe
 end
